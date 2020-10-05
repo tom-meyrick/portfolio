ac0672adb8b5e7b87863d7b69b309ddf56b8b2c1
Tom Meyrick <tomgmeyrick@gmail.com>
2020-10-05 15:18:32 +0100

    Added wave icon to contacr

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,14 +2,18 @@ import React from "react"
 import { Link } from "gatsby"
 import { navLinks, siteShortTitle } from "../config"
 import Image from "gatsby-image"
+import WaveSVG from "../content/icons/wave-hand-svgrepo-com.svg"
 
 const Projects = () => {
   return (
     <section id="contact">
-      <div class="container mx-auto text-md bg-blue-200 text-white rounded">
+      <div class="container mx-auto text-md bg-blue-100 rounded">
         <div class="pt-20 m-10">
           <div class="flex">
             <div class="w-1/2 text-6xl">Contact</div>
+            <div class="w-1/2 flex justify-end">
+              <img src={WaveSVG} class="w-25 h-25 mr-20"></img>
+            </div>
           </div>
         </div>
         <div class="m-10 pb-10 text-xl">
--- src/components/footer.js
+++ src/components/footer.js
@@ -7,8 +7,8 @@ import GithubSVG from "../content/icons/github.svg"
 
 const Footer = () => {
   return (
-    <footer className="flex bg-blue-100 container justify-end">
-      <div>
+    <footer className="flex bg-blue-200 container justify-center">
+      <div class="">
         <ul class="flex">
           <li>
             <img src={MailSVG} class="h-10 w-10 m-10 bg-white rounded"></img>
