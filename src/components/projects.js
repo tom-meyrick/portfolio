a3a84d4bb53273d0831a7aaa71852c9edf5f23f0
Tom Meyrick <tomgmeyrick@gmail.com>
2020-10-05 12:06:46 +0100

    Added links to project svgs

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 import { navLinks, siteShortTitle } from "../config"
 import Image from "gatsby-image"
 import Github from "../content/icons/github.svg"
+import LinkSVG from "../content/icons/link.svg"
 
 const Projects = ({ title, description, image, code, url }) => {
   const { menu } = navLinks
@@ -13,9 +14,12 @@ const Projects = ({ title, description, image, code, url }) => {
           <h2 class="font-bold">{title}</h2>
           <p class="m-5">{description}</p>
         </div>
-        <div class="p-10">
+        <div class="p-10 flex flex-end flex-row-reverse">
           <a href={code}>
-            <img src={Github} class="w-10 h-10"></img>
+            <img src={Github} class="w-10 h-10 mr-10"></img>
+          </a>
+          <a href={url}>
+            <img src={LinkSVG} class="w-10 h-10 mr-10"></img>
           </a>
         </div>
       </div>
