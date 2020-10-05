5596d23a35438a26dd9861985e40a07fa765f386
Tom Meyrick <tomgmeyrick@gmail.com>
2020-10-05 14:37:49 +0100

    Positioned links in footer

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,9 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 import { navLinks, siteShortTitle } from "../config"
 import Image from "gatsby-image"
-import MailSVG from "../content/icons/mail.svg"
-import LinkedInSVG from "../content/icons/linkedin.svg"
-import GithubSVG from "../content/icons/github.svg"
 
 const Projects = () => {
   return (
@@ -13,28 +10,6 @@ const Projects = () => {
         <div class="px-10 pt-20 m-10 flex flex-start flex-col">
           <div class="flex">
             <div class="w-1/2 text-6xl font-anton flex-start">Contact</div>
-            <div class="w-1/2 flex flex-row justify-end">
-              <ul class="flex pr-10">
-                <li>
-                  <img
-                    src={MailSVG}
-                    class="h-10 w-10 m-10 bg-white rounded"
-                  ></img>
-                </li>
-                <li>
-                  <img
-                    src={LinkedInSVG}
-                    class="h-10 w-10 m-10 bg-white rounded"
-                  ></img>
-                </li>
-                <li>
-                  <img
-                    src={GithubSVG}
-                    class="h-10 w-10 m-10 bg-white rounded"
-                  ></img>
-                </li>
-              </ul>
-            </div>
           </div>
         </div>
       </div>
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,30 @@
 import React from "react"
 import { Link } from "gatsby"
 import { siteShortTitle } from "../config/index"
+import MailSVG from "../content/icons/mail.svg"
+import LinkedInSVG from "../content/icons/linkedin.svg"
+import GithubSVG from "../content/icons/github.svg"
+
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-between bg-blue-100 py-6 px-12 container"></footer>
+    <footer className="flex bg-blue-100 container justify-end">
+      <div>
+        <ul class="flex">
+          <li>
+            <img src={MailSVG} class="h-10 w-10 m-10 bg-white rounded"></img>
+          </li>
+          <li>
+            <img
+              src={LinkedInSVG}
+              class="h-10 w-10 m-10 bg-white rounded"
+            ></img>
+          </li>
+          <li>
+            <img src={GithubSVG} class="h-10 w-10 m-10 bg-white rounded"></img>
+          </li>
+        </ul>
+      </div>
+    </footer>
   )
 }
 export default Footer
