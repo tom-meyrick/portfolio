06a111b2c010648354b9aef4dcc6aee8ac35ae45
Tom Meyrick <tomgmeyrick@gmail.com>
2020-10-06 16:34:40 +0100

    Added social media meta tags to helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,6 +23,39 @@ const Layout = ({ children }) => {
         />
         <meta name="author" content="Tom Meyrick" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* Facebook */}
+        <meta property="og:url" content="https://tom-meyrick.netlify.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Tom Meyrick: Full Stack Developer" />
+        <meta property="og:image" content="../content/images/tom_meyrick.png" />
+        <meta
+          property="og:image:alt"
+          content="Tom Meyrick is a full stack developer and recent DevelopMe graduate"
+        />
+        <meta
+          property="og:description"
+          content="Tom Meyrick is a full stack developer. Experienced in a full suite of frontend and backend tools and frameworks. Check out my most recent work here"
+        />
+        <meta property="og:site_name" content="Tom Meyrick" />
+        <meta property="og:locale" content="en_UK" />
+        {/* Twitter */}
+        <meta
+          name="twitter:card"
+          content="Tom Meyrick is a full stack developer. Experienced in a full suite of frontend and backend tools and frameworks. Check out my most recent work here"
+        />
+        <meta name="twitter:title" content="Tom Meyrick" />
+        <meta
+          name="twitter:description"
+          content="Tom Meyrick is a full stack developer. Experienced in a full suite of frontend and backend tools and frameworks. Check out my most recent work here"
+        />
+        <meta
+          name="twitter:image"
+          content="../content/images/tom_meyrick.png"
+        />
+        <meta
+          name="twitter:image:alt"
+          content="Tom Meyrick is a full stack developer and recent DevelopMe graduate"
+        />
       </Helmet>
       <Header />
       <main>{children}</main>
