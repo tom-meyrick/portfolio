057d9c4de730a210376014c1740e42e63140d04b
Tom Meyrick <tomgmeyrick@gmail.com>
2020-10-06 12:43:09 +0100

    Changed layout filename

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import Hero from "../components/hero"
 import Projects from "../components/projects"
 import Skills from "../components/skills"
